37cd7741c219613c00cb9d6af6a1f5dbaa8c9ffb
hdorgeval <henri.dorgeval@outlook.fr>
2016-11-07 01:00:53 +0100

    Framework 3.5 Extensions  NuGet setup

--- aaaProgramming/Framework 3.5 Extensions/Properties/AssemblyInfo.cs
+++ aaaProgramming/Framework 3.5 Extensions/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Framework 3.5 Extensions")]
-[assembly: AssemblyDescription("aaaProgramming")]
+[assembly: AssemblyDescription("Set of Framework Extensions to help you make a cleaner code and deliver high quality code more rapidly.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Henri d'Orgeval")]
 [assembly: AssemblyProduct("Framework 3.5 Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -33,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0-beta")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
